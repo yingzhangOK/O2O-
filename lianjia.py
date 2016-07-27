24704d2d4629ebc50e38c5c8461e9dc1a3d03b98
heruihong@hotmail.com <heruihong@hotmail.com>
2016-07-27 18:01:24 +0800

    lianjia

--- lianjia.py
+++ lianjia.py
@@ -40,7 +40,26 @@ def get_html_text(url):
 	return soup
 
 
-def get_area_list(soup):
+def get_sub_area_list(soup):
+	if not soup:
+		return None
+
+	ret = soup.find_all('div', 'option-list sub-option-list')
+
+	sub_area_list = dict()
+	if ret and len(ret) > 0:
+		sub_filter_soup = BS(str(ret[0]), 'lxml')
+		soup_sub_area_list = sub_filter_soup.select('a')
+		for i in xrange(1, len(soup_sub_area_list)):
+			soup_tmp  = BS(str(soup_sub_area_list[i]), 'lxml')
+			k = soup_tmp.a.attrs['href']
+			v = soup_tmp.select('a')[0].get_text()
+			sub_area_list[k] = v
+
+	return sub_area_list
+
+
+def get_house_base_info(soup, area):
 	ul = soup.find_all('ul', class_ = 'house-lst')
 	# print 'ul: %s' % ul
 	ret = list()
@@ -64,50 +83,37 @@ def get_area_list(soup):
 	return ret
 
 
-def get_sub_area_list(soup):
-	if not soup:
-		return None
-
-	ret = soup.find_all('div', 'option-list sub-option-list')
-
-	sub_area_list = dict()
-	if ret and len(ret) > 0:
-		sub_filter_soup = BS(str(ret[0]), 'lxml')
-		soup_sub_area_list = sub_filter_soup.select('a')
-		for i in xrange(1, len(soup_sub_area_list)):
-			soup_tmp  = BS(str(soup_sub_area_list[i]), 'lxml')
-			k = soup_tmp.a.attrs['href']
-			v = soup_tmp.select('a')[0].get_text()
-			sub_area_list[k] = v
-
-	return sub_area_list
+def get_house_detail():
+	pass
 
 
+def get_house_location():
+	pass
 
 
 def get_fin_url(sub_area_list):
 	pass
 
 
-if __name__ == '__main__':
+def get_base_data():
 	erf_url = base_url + '/ershoufang'
 	base_soup = get_html_text(erf_url)
 	# area_list = get_area_list(base_soup)
-	area_list = {'/ershoufang/yubei/': u'\xe6\xb8\x9d\xe5\x8c\x97', '/ershoufang/changshou1/': u'\xe9\x95\xbf\xe5\xaf\xbf', 
-				 '/ershoufang/yongchuan/': u'\xe6\xb0\xb8\xe5\xb7\x9d', '/ershoufang/jiulongpo/': u'\xe4\xb9\x9d\xe9\xbe\x99\xe5\x9d\xa1', 
-				 '/ershoufang/fuling/': u'\xe6\xb6\xaa\xe9\x99\xb5', '/ershoufang/qijiang/': u'\xe7\xb6\xa6\xe6\xb1\x9f', 
-				 '/ershoufang/banan/': u'\xe5\xb7\xb4\xe5\x8d\x97', '/ershoufang/nanan/': u'\xe5\x8d\x97\xe5\xb2\xb8', 
-				 '/ershoufang/jiangjing/': u'\xe6\xb1\x9f\xe6\xb4\xa5', '/ershoufang/shapingba/': u'\xe6\xb2\x99\xe5\x9d\xaa\xe5\x9d\x9d', 
-				 '/ershoufang/fengdu1/': u'\xe4\xb8\xb0\xe9\x83\xbd', '/ershoufang/yuzhong/': u'\xe6\xb8\x9d\xe4\xb8\xad', 
-				 '/ershoufang/dadukou/': u'\xe5\xa4\xa7\xe6\xb8\xa1\xe5\x8f\xa3', '/ershoufang/jiangbei/': u'\xe6\xb1\x9f\xe5\x8c\x97'}
+	area_list = {'/ershoufang/yubei/': '\xe6\xb8\x9d\xe5\x8c\x97', '/ershoufang/changshou1/': '\xe9\x95\xbf\xe5\xaf\xbf', 
+				 '/ershoufang/yongchuan/': '\xe6\xb0\xb8\xe5\xb7\x9d', '/ershoufang/jiulongpo/': '\xe4\xb9\x9d\xe9\xbe\x99\xe5\x9d\xa1', 
+				 '/ershoufang/fuling/': '\xe6\xb6\xaa\xe9\x99\xb5', '/ershoufang/qijiang/': '\xe7\xb6\xa6\xe6\xb1\x9f', 
+				 '/ershoufang/banan/': '\xe5\xb7\xb4\xe5\x8d\x97', '/ershoufang/nanan/': '\xe5\x8d\x97\xe5\xb2\xb8', 
+				 '/ershoufang/jiangjing/': '\xe6\xb1\x9f\xe6\xb4\xa5', '/ershoufang/shapingba/': '\xe6\xb2\x99\xe5\x9d\xaa\xe5\x9d\x9d', 
+				 '/ershoufang/fengdu1/': '\xe4\xb8\xb0\xe9\x83\xbd', '/ershoufang/yuzhong/': '\xe6\xb8\x9d\xe4\xb8\xad', 
+				 '/ershoufang/dadukou/': '\xe5\xa4\xa7\xe6\xb8\xa1\xe5\x8f\xa3', '/ershoufang/jiangbei/': '\xe6\xb1\x9f\xe5\x8c\x97'}
+
+	# for a in area_list:
+	# 	area_list[a] = area_list[a].decode('utf-8')
 
 	sub_area_list = list()
 	for k, v in area_list.items():
 		url = base_url + k
 		tmp_soup = get_html_text(url)
-		xxoo = get_area_list(tmp_soup)
-		if len(xxoo) == 0:
-			continue
 			
 		tmp_sub_area_list = get_sub_area_list(tmp_soup)
 		sub_area_list.append(tmp_sub_area_list)
@@ -132,4 +138,31 @@ if __name__ == '__main__':
 
 	if len(fin_list) > 0:
 		df = pd.DataFrame(fin_list)
-		df.to_csv('cq_lj_base_data.csv', encoding = 'utf-8')
+		df.to_csv('cq_lj_base_data_1.csv', encoding = 'utf-8')
+
+
+if __name__ == '__main__':
+	valid_data = pd.read_csv('cq_lj_base_data.csv')
+	# urls = data[data.total_page > 0]['url']
+	# valid_data = base_data[base_data.total_page > 0]
+
+	ret = list()
+	for i in range(len(valid_data)):
+		item = valid_data.ix[i]
+		uri = item['url']
+		
+		total_page = int(item['total_page'])
+		if total_page == 0:
+			continue
+
+		for j in range(total_page):
+			print 'Proccessing page %s of %s' % (j+1, uri)
+			page = 'pg%s' % (i+1)
+			url = base_url + uri + page
+			soup = get_html_text(url)
+			tmp_ret = get_house_base_info(soup, url)
+			ret.extend(tmp_ret)
+
+	if len(ret) > 0:
+		df = pd.DataFrame(ret)
+		df.to_csv('cq_lj_base_info.csv')
